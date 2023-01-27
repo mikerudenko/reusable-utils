d6469ac0c42fedc2abc89b9deb99b5a0faf03c4b
Mike Rudenko <mihailo.rudenko.main@gmail.com>
2023-01-27 19:25:02 +0200

    add reusable code and component from old projects

---
+++ fp-utils/decorators.js
@@ -0,0 +1,17 @@
+
+function decorateArgumentsWith() {
+    var decorators = [].slice.call(arguments);
+    /*jshint validthis: true */
+    var subject = this;
+
+    function decorate(args) {
+        return args.map(function(arg, i) {
+            return decorators[i](arg);
+        });
+    }
+
+    return function() {
+        return subject.apply(this, decorate([].slice.call(arguments)));
+    };
+}
+
---
+++ fp-utils/negate.js
@@ -0,0 +1,19 @@
+/**
+ * @example
+ *
+ * function isEven(number) {
+ *   return number % 2;
+ * }
+ *
+ * var isOdd = negate(isEven);
+ *
+ * isEven(2); // return true
+ * isOdd(2);  // return false
+ * 
+ */
+
+function negate(subject) {
+    return function() {
+        return !subject.apply(this, arguments);
+    };
+}
